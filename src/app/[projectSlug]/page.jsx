138ac40d333b3f14e0760cf668891627baa2ea7e
rafalziolek <90690517+rafalziolek@users.noreply.github.com>
2023-08-31 17:17:50 +0200

    Update page.jsx

--- src/app/[projectSlug]/page.jsx
+++ src/app/[projectSlug]/page.jsx
@@ -1,5 +1,5 @@
 import { MDXRemote } from "next-mdx-remote/rsc";
-import ProjectHeader from "./components/ProjectHeader/ProjectHeader";
+// import ProjectHeader from "./components/ProjectHeader/ProjectHeader";
 import { loadBlogPost } from "@/helpers/file-helpers";
 
 async function ProjectPage({ params }) {
@@ -7,12 +7,12 @@ async function ProjectPage({ params }) {
 
   return (
     <div>
-      <ProjectHeader
+      {/* <ProjectHeader
         title={frontmatter.title}
         abstract={frontmatter.abstract}
         details={frontmatter.details}
         live={frontmatter.live}
-      ></ProjectHeader>
+      ></ProjectHeader> */}
       <MDXRemote source={content} />
     </div>
   );
