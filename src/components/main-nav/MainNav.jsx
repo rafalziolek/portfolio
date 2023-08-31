4db06177209aaab1e6f9c80ce81a9bd285c97763
rafalziolek <90690517+rafalziolek@users.noreply.github.com>
2023-08-31 17:06:34 +0200

    Update MainNav.jsx

--- src/components/main-nav/MainNav.jsx
+++ src/components/main-nav/MainNav.jsx
@@ -12,7 +12,7 @@ function MainNav() {
         </Link>
       </div>
       <ul>
-        <NavLink href="/bio">Bio</NavLink>
+        <NavLink href="/">Bio</NavLink>
         <NavLink href="#Work">Work</NavLink>
       </ul>
     </nav>
