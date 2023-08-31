841c9549de5c77859ef612203bff5d6062104880
rafalziolek <90690517+rafalziolek@users.noreply.github.com>
2023-08-31 17:44:46 +0200

    test4

--- src/app/[projectSlug]/page.jsx
+++ src/app/[projectSlug]/page.jsx
@@ -1,6 +1,7 @@
+import React from "react";
 import { MDXRemote } from "next-mdx-remote/rsc";
 import ProjectHeader from "./components/ProjectHeader/ProjectHeader";
-import loadBlogPost from "@/helpers/file-helpers";
+import { loadBlogPost } from "@/helpers/file-helpers";
 
 async function ProjectPage({ params }) {
   const { frontmatter, content } = await loadBlogPost(params.projectSlug);
--- src/helpers/file-helpers.js
+++ src/helpers/file-helpers.js
@@ -2,7 +2,7 @@ import fs from "fs/promises";
 import path from "path";
 import matter from "gray-matter";
 
-export default async function getBlogPostList() {
+export async function getBlogPostList() {
   const fileNames = await readDirectory("/content");
 
   const blogPosts = [];
