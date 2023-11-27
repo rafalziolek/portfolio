400518b3fd9ccff7d9ee8288354afe02ef1ecdbb
rafalziolek <90690517+rafalziolek@users.noreply.github.com>
2023-11-27 21:10:01 +0100

    some improvements

--- src/components/main-nav/MainNav.jsx
+++ src/components/main-nav/MainNav.jsx
@@ -6,35 +6,52 @@ import Grid from "@/components/Grid/Grid";
 import GridItem from "@/components/Grid/GridItem";
 import MainNavItem from "./MainNavItem";
 import CustomLink from "../custom-link/CustomLink";
+import Text from "../Text/text";
 
 function MainNav() {
   return (
     <nav className={styles["main-nav"]}>
       <Grid align="center">
         <GridItem columnSpan={2}>
-          <Logo />
+          <Stack direction="column" gap="xxs">
+            <Text type="caption" color="secondary">
+              Name
+            </Text>
+            <Logo />
+          </Stack>
         </GridItem>
         <GridItem columnSpan={8}>
-          <ul>
-            <MainNavItem glyphLetter="W" href="/work" glyphColor="blue">
-              Work,
-            </MainNavItem>
-            <MainNavItem glyphLetter="A" href="/about" glyphColor="orange">
-              About
-            </MainNavItem>
-          </ul>
+          <Stack direction="column" gap="xxs">
+            <Text type="caption" color="secondary">
+              Menu
+            </Text>
+            <ul>
+              <MainNavItem glyphLetter="W" href="/work" glyphColor="blue">
+                Work,
+              </MainNavItem>
+              <MainNavItem glyphLetter="A" href="/about" glyphColor="orange">
+                About
+              </MainNavItem>
+            </ul>
+          </Stack>
         </GridItem>
         <GridItem
           columnSpan={-3}
           justifySelf="end"
           style={{ display: "flex", justifyContent: "end" }}
         >
-          <a
-            className={`${styles["main-nav-link"]} ${styles.mail}`}
-            href="mailto://rafal.ziolek@icloud.com"
-          >
-            rafal.ziolek@icloud.com
-          </a>
+          <Stack direction="column" gap="xxs">
+            {" "}
+            <Text type="caption" color="secondary">
+              Contact
+            </Text>
+            <a
+              className={`${styles["main-nav-link"]} ${styles.mail}`}
+              href="mailto://rafal.ziolek@icloud.com"
+            >
+              rafal.ziolek@icloud.com
+            </a>
+          </Stack>
         </GridItem>
       </Grid>
     </nav>
@@ -43,16 +60,8 @@ function MainNav() {
 
 export function Logo() {
   return (
-    <div>
-      <Link href="/" className={styles["logo"]}>
-        Rafał Ziółek
-      </Link>
-      <Link
-        href="/"
-        className={`${styles["nav-link"]} ${styles["logo-initials"]}`}
-      >
-        RZ
-      </Link>
+    <div className={styles["logo"]}>
+      <Link href="/">Rafał Ziółek</Link>
     </div>
   );
 }
