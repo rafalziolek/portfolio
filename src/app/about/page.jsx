7f5d674fc7b93031b51fa1ee61a9d0d64899555f
rafalziolek <90690517+rafalziolek@users.noreply.github.com>
2023-12-06 00:15:55 +0100

    Update page.jsx

--- src/app/about/page.jsx
+++ src/app/about/page.jsx
@@ -1,18 +1,18 @@
-import styles from "./page.module.scss";
-import AboutHeader from "./Components/AboutHeader";
-import List from "@/components/List/List";
-import { Stack, StackItem } from "@/components/Stack/Stack";
-import Text from "@/components/Text/text";
-import AboutSection from "./Components/AboutSection/AboutSection";
-import ProfilePhoto from "./Components/ProfilePhoto/ProfilePhoto";
+import styles from './page.module.scss';
+import AboutHeader from './Components/AboutHeader';
+import List from '@/components/List/List';
+import { Stack, StackItem } from '@/components/Stack/Stack';
+import Text from '@/components/Text/text';
+import AboutSection from './Components/AboutSection/AboutSection';
+import ProfilePhoto from './Components/ProfilePhoto/ProfilePhoto';
 function Bio() {
   return (
-    <>
-      <AboutHeader />
+    <div style={{ position: 'relative', overflow: 'hidden' }}>
       <ProfilePhoto />
-      <AboutSection title={{ title: "My approach", number: "1" }}>
-        <Stack gap="l" wrap="wrap">
-          <Text className={styles["section-text"]}>
+      <AboutHeader />
+      <AboutSection title={{ title: 'My approach', number: '1' }}>
+        <Stack gap='l' wrap='wrap'>
+          <Text className={styles['section-text']}>
             I'm a designer who loves getting into both the nitty-gritty details
             and the big picture.
             <br />
@@ -26,7 +26,7 @@ function Bio() {
             these feed into a design process that's as versatile as it is
             focused.
           </Text>
-          <Text className={styles["section-text"]}>
+          <Text className={styles['section-text']}>
             Although I am first and foremost a designer, I do experiment with
             code to give my designs an extra layer of polish. <br />
             <br />
@@ -36,40 +36,40 @@ function Bio() {
           </Text>
         </Stack>
       </AboutSection>
-      <AboutSection title={{ title: "Who inspires me", number: "2" }}>
-        <Stack direction="column" gap="xxs">
-          <a href="https://www.youtube.com/@FaizalWestcott" target="_blank">
+      <AboutSection title={{ title: 'Who inspires me', number: '2' }}>
+        <Stack direction='column' gap='xxs'>
+          <a href='https://www.youtube.com/@FaizalWestcott' target='_blank'>
             Faizal Westcott ↗︎
           </a>
-          <a href="https://twitter.com/rsms" target="_blank">
+          <a href='https://twitter.com/rsms' target='_blank'>
             Rasmsus Andersson ↗︎
           </a>
-          <a href="https://twitter.com/blankresident" target="_blank">
+          <a href='https://twitter.com/blankresident' target='_blank'>
             Farzad Ban ↗︎
           </a>
           <a
-            href="https://music.apple.com/us/artist/tyler-the-creator/420368335"
-            target="_blank"
+            href='https://music.apple.com/us/artist/tyler-the-creator/420368335'
+            target='_blank'
           >
             Tyler The Creator ↗︎
           </a>
         </Stack>
       </AboutSection>
-      <AboutSection title={{ title: "Learning now", number: "3" }}>
-        <Stack direction="column" gap="xxs">
+      <AboutSection title={{ title: 'Learning now', number: '3' }}>
+        <Stack direction='column' gap='xxs'>
           <Text>SwiftUI</Text>
           <Text>React</Text>
           <Text>Designing clothes</Text>
         </Stack>
       </AboutSection>
-      <AboutSection title={{ title: "Random favs", number: "4" }}>
-        <Stack direction="column" gap="xxs">
+      <AboutSection title={{ title: 'Random favs', number: '4' }}>
+        <Stack direction='column' gap='xxs'>
           <Text>Star Wars</Text>
           <Text>To Pimp a Butterfly</Text>
           <Text>Cooking</Text>
         </Stack>
       </AboutSection>
-    </>
+    </div>
   );
 }
 
